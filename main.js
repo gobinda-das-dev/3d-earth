4412c4704b3b24aadf1da80ba233971d5c1f039c
Gobinda Das <gobinda993720@gmail.com>
2024-06-25 22:46:52 -0700

    handle screen resize

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ import atmosphereVertexShader from './shader/atmosphereVertex.glsl';
 import atmosphereFragmentShader from './shader/atmosphereFragment.glsl';
 import gsap from 'gsap';
 
-
 const { innerWidth, innerHeight } = window;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -19,10 +18,10 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setPixelRatio(2)
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
 const sphere = new THREE.Mesh(
@@ -65,16 +64,14 @@ scene.add(stars);
 const numStars = 1000;
 const starVertices = [];
 for (let i = 0; i < numStars; i++) {
-  const x = gsap.utils.random(-innerWidth/2, innerWidth/2, 0.01);
-  const y = gsap.utils.random(-innerWidth/2, innerWidth/2, 0.01);
-  const z = gsap.utils.random(-innerWidth/2, innerWidth/2, 0.01);
+  const x = gsap.utils.random(-innerWidth / 2, innerWidth / 2, 0.01);
+  const y = gsap.utils.random(-innerWidth / 2, innerWidth / 2, 0.01);
+  const z = gsap.utils.random(-innerWidth / 2, innerWidth / 2, 0.01);
 
   starVertices.push(x, y, z);
-  
 }
 
-starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3))
-
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3));
 
 const mouse = {
   x: null,
@@ -98,9 +95,14 @@ function animate() {
 }
 animate();
 
-window.addEventListener('mousemove', ({x, y}) => {
+window.addEventListener('mousemove', ({ x, y }) => {
   mouse.x = (x / innerWidth) * 2 - 1;
   mouse.y = (y / innerHeight) * 2 + 1;
+});
 
-  // console.log(mouse);
-})
+window.addEventListener('resize', () => {
+  const { innerWidth, innerHeight } = window;
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(innerWidth, innerHeight);
+});
